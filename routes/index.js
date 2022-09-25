a4830e4e9dc72e894ca25c161d269ee7a8509260
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2022-09-25 22:55:45 +0530

    passport added

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ const homeContoller = require("../controllers/home_contoller");
 
 router.get('/',homeContoller.home);
 router.use('/users',require('./users'));
-router.use('/users',require('./signup'));
-router.use('/users',require('./login'));
+
 //for any further routes it can be used form here -- router.name('/file',require(./filename));
 // console.log('router is loaded');
 
