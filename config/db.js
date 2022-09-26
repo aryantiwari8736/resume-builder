3b5d0577374fdb0507570c01df64bfb97a865af9
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2022-09-27 04:20:36 +0530

    atlas is connected

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://localhost:27017";
+const mongoURI =  "mongodb+srv://aryantiwari:sanu1234@cluster0.0qa2gqs.mongodb.net/?retryWrites=true&w=majority";
 const connectToMongo = () =>{
     mongoose.connect(mongoURI,()=>{
         console.log("MongoDb is connected succesfully ");
