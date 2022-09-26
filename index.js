55cea7e765f7f52223c248725423086a5d81f7a0
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2022-09-27 04:02:45 +0530

    added dynamic port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express');
 const env = require('./config/enviornment');
 const cookieParser = require('cookie-parser')
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 var flash = require('connect-flash');
 
