23023edeee2d6e783d77dfeb3dad618e08835c64
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2022-09-26 21:04:51 +0530

    final version 1.0

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb+srv://aryantiwari:sanu1234@cluster0.0qa2gqs.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI =  "mongodb://localhost:27017";
 const connectToMongo = () =>{
     mongoose.connect(mongoURI,()=>{
         console.log("MongoDb is connected succesfully ");
